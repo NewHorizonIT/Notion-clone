f65a946da1f2720ca53a471cd42809579a6e300a
AnhQuan <nguyenanhquandev@gmail.com>
2025-11-02 19:32:18 +0700

    feature(block): add api of block

---
+++ notion-be/src/controllers/block.controller.ts
@@ -0,0 +1,65 @@
+import { injectable } from "tsyringe";
+import { CreateBlockData } from "../schemas/blog.schema";
+import BlockService from "../services/block.service";
+import { SuccessResponse } from "../response/response";
+import { StatusCodes } from "../response";
+import { Request, Response } from "express";
+import { Queries } from "../response/query";
+
+@injectable()
+class BlockController {
+  constructor(private readonly blockService: BlockService) {}
+  // Create new Block
+  createBlock = async (req: Request, res: Response): Promise<void> => {
+    const blockData = req.body as CreateBlockData;
+    const block = await this.blockService.createNewBlock(blockData);
+
+    new SuccessResponse({
+      statusCode: StatusCodes.OK,
+      message: "Create block Success",
+      data: block,
+    }).send(res);
+  };
+
+  // Get block By ID
+  getBlockByID = async (req: Request, res: Response): Promise<void> => {
+    const blockID = req.params.id as string;
+    const block = await this.blockService.getBlockByID(blockID);
+
+    new SuccessResponse({
+      statusCode: StatusCodes.OK,
+      message: "Get block success",
+      data: block,
+    }).send(res);
+  };
+
+  // Get block of Page
+  getBlockOfPage = async (req: Request, res: Response): Promise<void> => {
+    const queries = req.query as Queries;
+    const pageID = req.params.id;
+    const blocks = await this.blockService.getBlockByPage(pageID, queries);
+
+    new SuccessResponse({
+      message: "Get blocks of page success",
+      statusCode: StatusCodes.OK,
+      data: blocks,
+    }).send(res);
+  };
+
+  // Get children block
+  getChildrenBlocksOfParent = async (
+    req: Request,
+    res: Response,
+  ): Promise<void> => {
+    const parentID = req.params.id as string;
+    const blocks = await this.blockService.getChildrenOfBlock(parentID);
+
+    new SuccessResponse({
+      message: "Get blocks of parrent success",
+      statusCode: StatusCodes.OK,
+      data: blocks,
+    }).send(res);
+  };
+}
+
+export default BlockController;
